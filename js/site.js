236e90d4811f2ea589f0afb823b854c7cc2b558f
Matt Johnson <matt_i_johnson@hotmail.com>
2023-09-08 11:13:29 +0100

    Demo version

---
+++ js/site.js
@@ -0,0 +1,19 @@
+function changeVetArtList() {
+    //alert('Changed to' + document.getElementById('vetartList').value)
+    var arr = ['Vet Surgery 1', 'Vet Surgery 2', 'Vet Surgery 3', 'Vet Surgery 4', 'Vet Surgery 5'];
+    var cont = document.getElementById('vetartListResult');
+    cont.replaceChildren();
+    
+    var ul = document.createElement('ul');
+    ul.setAttribute('id', 'theList');
+    var randomLength = Math.floor(Math.random() * 6) + 1;
+    for (i = 0; i <= randomLength - 1; i++) {
+    // for (i = 0; i <= arr.length - 1; i++) {
+        var li = document.createElement('li');
+        li.innerHTML = arr[i];
+
+        ul.appendChild(li);
+    }
+
+    cont.appendChild(ul);
+}
